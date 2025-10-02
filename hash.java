00cf1f1330a78477e9864d61a81c1bf327cf3a7b
kamapulya227 <kim.ase4ka@yandex.ru>
2025-10-02 15:31:30 +0300

    Update hash.java

--- hash.java
+++ hash.java
@@ -3,13 +3,13 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, Double> ht = new HashMap<>();
-        ht.put("Температура", 23.5);
-        ht.put("Процент", 87.2);
-        ht.put("Скидка", 15.0);
+        Map<String, Boolean> ht = new HashMap<>();
+        ht.put("Фонарь", true);
+        ht.put("Дверь", false);
+        ht.put("Окно", true);
 
         for (var entry : ht.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+            System.out.println(entry.getKey() + ": " + (entry.getValue() ? "включен" : "выключен"));
         }
     }
 }
